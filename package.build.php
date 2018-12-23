03375ce56af72081a2583e9c8e051e3dd348cb21
MWGuy <yousan4ik@gmail.com>
2018-12-23 17:33:48 +0300

    travis auto hub:publish --yes

--- package.build.php
+++ package.build.php
@@ -2,9 +2,23 @@
 
 use packager\Event;
 use packager\cli\Console;
+use php\io\Stream;
 
 function task_build(Event $e)
 {
-    foreach ($e->package()->getAny("modules") as $module => $task)
-    	Tasks::runExternal('./' . $module, $task, [], "yes");
+    foreach ($e->package()->getAny("modules") as $module => $options)
+    	Tasks::runExternal('./' . $module, $options["task"], [], "yes");
+}
+
+function task_hubBuild(Event $e)
+{
+	foreach ($e->package()->getAny("modules") as $module => $options)
+	{
+    	if (!$options["hub"]) continue;
+
+    	Stream::putContents('./' . $module . "/package.hub.yml",
+    		"login: " . $GLOBALS['argv'][2] . "\nauth: " . $GLOBALS['argv'][3]);
+    	
+    	Tasks::runExternal('./' . $module, "hub:publish", [], "yes");
+	}
 }
