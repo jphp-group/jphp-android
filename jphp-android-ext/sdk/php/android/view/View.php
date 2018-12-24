7f0ab8e347a34b2dc456c0ec7bf67105f47afd59
MWGuy <yousan4ik@gmail.com>
2018-12-25 00:27:58 +0300

    update

--- jphp-android-ext/sdk/php/android/view/View.php
+++ jphp-android-ext/sdk/php/android/view/View.php
@@ -6,6 +6,61 @@ use php\android\content\Context;
 
 class View
 {
+    public const NO_ID = -1;
+    public const VISIBLE = 0;
+    public const INVISIBLE = 4;
+    public const GONE = 8;
+    public const DRAWING_CACHE_QUALITY_LOW = 524288;
+    public const DRAWING_CACHE_QUALITY_HIGH = 1048576;
+    public const DRAWING_CACHE_QUALITY_AUTO = 0;
+    public const SCROLLBARS_INSIDE_OVERLAY = 0;
+    public const SCROLLBARS_INSIDE_INSET = 16777216;
+    public const SCROLLBARS_OUTSIDE_OVERLAY = 33554432;
+    public const SCROLLBARS_OUTSIDE_INSET = 50331648;
+    public const KEEP_SCREEN_ON = 67108864;
+    public const SOUND_EFFECTS_ENABLED = 134217728;
+    public const HAPTIC_FEEDBACK_ENABLED = 268435456;
+    public const FOCUSABLES_ALL = 0;
+    public const FOCUSABLES_TOUCH_MODE = 1;
+    public const FOCUS_BACKWARD = 1;
+    public const FOCUS_FORWARD = 2;
+    public const FOCUS_LEFT = 17;
+    public const FOCUS_UP = 33;
+    public const FOCUS_RIGHT = 66;
+    public const FOCUS_DOWN = 130;
+    public const MEASURED_SIZE_MASK = 16777215;
+    public const MEASURED_STATE_MASK = -16777216;
+    public const MEASURED_HEIGHT_STATE_SHIFT = 16;
+    public const MEASURED_STATE_TOO_SMALL = 16777216;
+    
+    public const TEXT_ALIGNMENT_INHERIT = 0;
+    public const TEXT_ALIGNMENT_RESOLVED_DEFAULT = 131072;
+    public const IMPORTANT_FOR_ACCESSIBILITY_AUTO = 0;
+    public const IMPORTANT_FOR_ACCESSIBILITY_YES = 1;
+    public const IMPORTANT_FOR_ACCESSIBILITY_NO = 2;
+    public const OVER_SCROLL_ALWAYS = 0;
+    public const OVER_SCROLL_IF_CONTENT_SCROLLS = 1;
+    public const OVER_SCROLL_NEVER = 2;
+    public const SYSTEM_UI_FLAG_VISIBLE = 0;
+    public const SYSTEM_UI_FLAG_LOW_PROFILE = 1;
+    public const SYSTEM_UI_FLAG_HIDE_NAVIGATION = 2;
+    public const SYSTEM_UI_FLAG_FULLSCREEN = 4;
+    public const SYSTEM_UI_FLAG_LAYOUT_STABLE = 256;
+    public const SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION = 512;
+    public const SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN = 1024;
+    
+    public const SYSTEM_UI_LAYOUT_FLAGS = 1536;
+    public const FIND_VIEWS_WITH_TEXT = 1;
+    public const FIND_VIEWS_WITH_CONTENT_DESCRIPTION = 2;
+    public const SCREEN_STATE_OFF = 0;
+    public const SCREEN_STATE_ON = 1;
+    public const SCROLLBAR_POSITION_DEFAULT = 0;
+    public const SCROLLBAR_POSITION_LEFT = 1;
+    public const SCROLLBAR_POSITION_RIGHT = 2;
+    public const LAYER_TYPE_NONE = 0;
+    public const LAYER_TYPE_SOFTWARE = 1;
+    public const LAYER_TYPE_HARDWARE = 2;
+    
     public function __construct(Context $activity)
     {
 
@@ -30,6 +85,201 @@ class View
      * @param string $event
      */
     public function trigger(string $event) {
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getVerticalFadingEdgeLength(): int {
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getHorizontalFadingEdgeLength(): int {
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getVerticalScrollbarWidth(): int {
+
+    }
+
+    /**
+     * @param int $length
+     */
+    public function setFadingEdgeLength(int $length) {
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function showContextMenu(): bool {
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasFocus(): bool {
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasFocusable(): bool {
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function isFocused(): bool {
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function isShown(): bool {
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasTransientState(): bool {
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function willNotDraw(): bool {
+
+    }
+
+    /**
+     * @param View $focused
+     * @param int $direction
+     * @return bool
+     */
+    public function dispatchUnhandledMove(View $focused, int $direction): bool {
+
+    }
+
+    /**
+     * @param int|null $direction
+     * @return bool
+     */
+    public function requestFocus(int $direction = null): bool {
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function requestFocusFromTouch(): bool {
+
+    }
+
+    /**
+     * @return View
+     */
+    public function findFocus(): View {
+
+    }
+
+    public function requestFitSystemWindows(): void {
+
+    }
+
+    /**
+     * @param bool $focusable
+     */
+    public function setFocusable(bool $focusable): void {
+
+    }
+
+    /**
+     * @param bool $focusableInTouchMode
+     */
+    public function setFocusableInTouchMode(bool $focusableInTouchMode): void {
+
+    }
+
+    /**
+     * @param bool $hasTransientState
+     */
+    public function setHasTransientState(bool $hasTransientState): void {
+
+    }
+
+    /**
+     * @param bool $willNotDraw
+     */
+    public function setWillNotDraw(bool $willNotDraw): void {
+
+    }
+
+    /**
+     * @param bool $willNotCacheDrawing
+     */
+    public function setWillNotCacheDrawing(bool $willNotCacheDrawing): void {
+
+    }
+
+    /**
+     * @param View[] $views
+     * @param int $direction
+     * @param int|null $focusableMode
+     */
+    public function addFocusables(array $views, int $direction, int $focusableMode = null): void {
+
+    }
+
+    /**
+     * @param View[] $outViews
+     * @param string $searched
+     * @param int $flags
+     */
+    public function findViewsWithText(array $outViews, string $searched, int $flags): void {
+
+    }
+
+    /**
+     * @param array $views
+     */
+    public function addTouchables(array $views): void {
+
+    }
+
+    /**
+     * @return View[]
+     */
+    public function getTouchables(): array {
+
+    }
+
+    /**
+     * @param int $direction
+     * @return View
+     */
+    public function focusSearch(int $direction): View {
+
+    }
+
+    /**
+     * @param int $direction
+     * @return View[]
+     */
+    public function getFocusables(int $direction): array {
 
     }
 
@@ -62,4 +312,114 @@ class View
      * @var float
      */
     public $rotation;
+
+    /**
+     * @var int
+     */
+    public $verticalScrollbarPosition;
+
+    /**
+     * @var int
+     */
+    public $drawingCacheQuality;
+
+    /**
+     * @var int
+     */
+    public $nextFocusLeftId;
+
+    /**
+     * @var int
+     */
+    public $nextFocusRightId;
+
+    /**
+     * @var int
+     */
+    public $nextFocusUpId;
+
+    /**
+     * @var int
+     */
+    public $nextFocusDownId;
+
+    /**
+     * @var int
+     */
+    public $nextFocusForwardId;
+
+    /**
+     * @var int
+     */
+    public $visibility;
+
+    /**
+     * @var int
+     */
+    public $importantForAccessibility;
+
+    /**
+     * @var string
+     */
+    public $contentDescription;
+
+    /**
+     * @var bool
+     */
+    public $scrollContainer;
+
+    /**
+     * @var bool
+     */
+    public $keepScreenOn;
+
+    /**
+     * @var bool
+     */
+    public $fitsSystemWindows;
+
+    /**
+     * @var bool
+     */
+    public $enabled;
+
+    /**
+     * @var bool
+     */
+    public $soundEffectsEnabled;
+
+    /**
+     * @var bool
+     */
+    public $hapticFeedbackEnabled;
+
+    /**
+     * @var bool
+     */
+    public $clickable;
+
+    /**
+     * @var bool
+     */
+    public $longClickable;
+
+    /**
+     * @var bool
+     */
+    public $pressed;
+
+    /**
+     * @var bool
+     */
+    public $saveEnabled;
+
+    /**
+     * @var bool
+     */
+    public $filterTouchesWhenObscured;
+
+    /**
+     * @var bool
+     */
+    public $saveFromParentEnabled;
 }
--- jphp-android-ext/src-jvm/main/java/org/venity/jphp/android/classes/view/WrapView.java
+++ jphp-android-ext/src-jvm/main/java/org/venity/jphp/android/classes/view/WrapView.java
@@ -10,11 +10,68 @@ import php.runtime.invoke.Invoker;
 import php.runtime.lang.BaseWrapper;
 import php.runtime.reflection.ClassEntity;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Reflection.Name("View")
 @Reflection.Namespace(AndroidExtension.VIEW_NS)
 public class WrapView extends BaseWrapper<View> {
+
+    public static final int NO_ID = -1;
+    public static final int VISIBLE = 0;
+    public static final int INVISIBLE = 4;
+    public static final int GONE = 8;
+    public static final int DRAWING_CACHE_QUALITY_LOW = 524288;
+    public static final int DRAWING_CACHE_QUALITY_HIGH = 1048576;
+    public static final int DRAWING_CACHE_QUALITY_AUTO = 0;
+    public static final int SCROLLBARS_INSIDE_OVERLAY = 0;
+    public static final int SCROLLBARS_INSIDE_INSET = 16777216;
+    public static final int SCROLLBARS_OUTSIDE_OVERLAY = 33554432;
+    public static final int SCROLLBARS_OUTSIDE_INSET = 50331648;
+    public static final int KEEP_SCREEN_ON = 67108864;
+    public static final int SOUND_EFFECTS_ENABLED = 134217728;
+    public static final int HAPTIC_FEEDBACK_ENABLED = 268435456;
+    public static final int FOCUSABLES_ALL = 0;
+    public static final int FOCUSABLES_TOUCH_MODE = 1;
+    public static final int FOCUS_BACKWARD = 1;
+    public static final int FOCUS_FORWARD = 2;
+    public static final int FOCUS_LEFT = 17;
+    public static final int FOCUS_UP = 33;
+    public static final int FOCUS_RIGHT = 66;
+    public static final int FOCUS_DOWN = 130;
+    public static final int MEASURED_SIZE_MASK = 16777215;
+    public static final int MEASURED_STATE_MASK = -16777216;
+    public static final int MEASURED_HEIGHT_STATE_SHIFT = 16;
+    public static final int MEASURED_STATE_TOO_SMALL = 16777216;
+
+    public static final int TEXT_ALIGNMENT_INHERIT = 0;
+    public static final int TEXT_ALIGNMENT_RESOLVED_DEFAULT = 131072;
+    public static final int IMPORTANT_FOR_ACCESSIBILITY_AUTO = 0;
+    public static final int IMPORTANT_FOR_ACCESSIBILITY_YES = 1;
+    public static final int IMPORTANT_FOR_ACCESSIBILITY_NO = 2;
+    public static final int OVER_SCROLL_ALWAYS = 0;
+    public static final int OVER_SCROLL_IF_CONTENT_SCROLLS = 1;
+    public static final int OVER_SCROLL_NEVER = 2;
+    public static final int SYSTEM_UI_FLAG_VISIBLE = 0;
+    public static final int SYSTEM_UI_FLAG_LOW_PROFILE = 1;
+    public static final int SYSTEM_UI_FLAG_HIDE_NAVIGATION = 2;
+    public static final int SYSTEM_UI_FLAG_FULLSCREEN = 4;
+    public static final int SYSTEM_UI_FLAG_LAYOUT_STABLE = 256;
+    public static final int SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION = 512;
+    public static final int SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN = 1024;
+
+    public static final int SYSTEM_UI_LAYOUT_FLAGS = 1536;
+    public static final int FIND_VIEWS_WITH_TEXT = 1;
+    public static final int FIND_VIEWS_WITH_CONTENT_DESCRIPTION = 2;
+    public static final int SCREEN_STATE_OFF = 0;
+    public static final int SCREEN_STATE_ON = 1;
+    public static final int SCROLLBAR_POSITION_DEFAULT = 0;
+    public static final int SCROLLBAR_POSITION_LEFT = 1;
+    public static final int SCROLLBAR_POSITION_RIGHT = 2;
+    public static final int LAYER_TYPE_NONE = 0;
+    public static final int LAYER_TYPE_SOFTWARE = 1;
+    public static final int LAYER_TYPE_HARDWARE = 2;
+
     protected final static AtomicInteger idCounter = new AtomicInteger(100000);
 
     public WrapView(Environment env, View wrappedObject) {
@@ -104,5 +161,61 @@ public class WrapView extends BaseWrapper<View> {
     @Reflection.Getter
     public float getRotation() {
         return getWrappedObject().getRotation();
+    }
+
+    public interface WrappedInterface {
+        int getVerticalFadingEdgeLength();
+        void setFadingEdgeLength(int length);
+        int getHorizontalFadingEdgeLength();
+        int getVerticalScrollbarWidth();
+        boolean showContextMenu();
+        boolean hasFocus();
+        boolean hasFocusable();
+        View findFocus();
+        boolean isFocused();
+        boolean isShown();
+        void requestFitSystemWindows();
+        void setFocusable(boolean focusable);
+        void setFocusableInTouchMode(boolean focusableInTouchMode);
+        boolean hasTransientState();
+        void setHasTransientState(boolean hasTransientState);
+        void setWillNotDraw(boolean willNotDraw);
+        boolean willNotDraw();
+        void setWillNotCacheDrawing(boolean willNotCacheDrawing);
+        boolean willNotCacheDrawing();
+        View focusSearch(int direction);
+        boolean dispatchUnhandledMove(View focused, int direction);
+        ArrayList<View> getFocusables(int direction);
+        void addFocusables(ArrayList<View> views, int direction);
+        void addFocusables(ArrayList<View> views, int direction, int focusableMode);
+        void findViewsWithText(ArrayList<View> outViews, CharSequence searched, int flags);
+        ArrayList<View> getTouchables();
+        void addTouchables(ArrayList<View> views);
+        boolean requestFocus();
+        boolean requestFocus(int direction);
+        boolean requestFocusFromTouch();
+
+        @Reflection.Property int verticalScrollbarPosition();
+        @Reflection.Property CharSequence contentDescription();
+        @Reflection.Property boolean scrollContainer();
+        @Reflection.Property int drawingCacheQuality();
+        @Reflection.Property boolean keepScreenOn();
+        @Reflection.Property int nextFocusLeftId();
+        @Reflection.Property int nextFocusRightId();
+        @Reflection.Property int nextFocusUpId();
+        @Reflection.Property int nextFocusDownId();
+        @Reflection.Property int nextFocusForwardId();
+        @Reflection.Property boolean fitsSystemWindows();
+        @Reflection.Property int visibility();
+        @Reflection.Property boolean enabled();
+        @Reflection.Property boolean soundEffectsEnabled();
+        @Reflection.Property boolean hapticFeedbackEnabled();
+        @Reflection.Property boolean clickable();
+        @Reflection.Property boolean longClickable();
+        @Reflection.Property boolean pressed();
+        @Reflection.Property boolean saveEnabled();
+        @Reflection.Property boolean filterTouchesWhenObscured();
+        @Reflection.Property boolean saveFromParentEnabled();
+        @Reflection.Property int importantForAccessibility();
     }
 }
