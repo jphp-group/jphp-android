0e6f5f87712ea8179bb675d6ec5202f08e26d286
MWGuy <yousan4ik@gmail.com>
2018-12-25 15:38:00 +0300

    update TextView

--- jphp-android-ext/sdk/php/android/widget/TextView.php
+++ jphp-android-ext/sdk/php/android/widget/TextView.php
@@ -15,4 +15,163 @@ class TextView extends View
      * @var int
      */
     public $inputType;
+
+    /**
+     * @var int
+     */
+    public $textColor;
+
+    /**
+     * @var int
+     */
+    public $highlightColor;
+
+    /**
+     * @var int
+     */
+    public $hintTextColor;
+
+    /**
+     * @var int
+     */
+    public $gravity;
+
+    /**
+     * @var int
+     */
+    public $paintFlags;
+
+    /**
+     * @var int
+     */
+    public $minLines;
+
+    /**
+     * @var int
+     */
+    public $minHeight;
+
+    /**
+     * @var int
+     */
+    public $maxLines;
+
+    /**
+     * @var int
+     */
+    public $maxHeight;
+
+    /**
+     * @var int
+     */
+    public $minWidth;
+
+    /**
+     * @var int
+     */
+    public $maxWidth;
+
+    /**
+     * @var int
+     */
+    public $minEms;
+
+    /**
+     * @var int
+     */
+    public $maxEms;
+
+    /**
+     * @var int
+     */
+    public $textSize;
+
+    /**
+     * @var int
+     */
+    public $textScaleX;
+
+    /**
+     * @var bool
+     */
+    public $freezesText;
+
+    /**
+     * @var bool
+     */
+    public $cursorVisible;
+
+    /**
+     * @var string
+     */
+    public $hint;
+
+    /**
+     * @return int
+     */
+    public function getShadowColor(): int {
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getLineSpacingMultiplier(): int {
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getLineSpacingExtra(): int {
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getShadowRadius(): int {
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getShadowDx(): int {
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getShadowDy(): int {
+
+    }
+
+    /**
+     * @param $text string
+     * @param $start int
+     * @param $end int
+     */
+    public function append(string $text, int $start, int $end): void {
+
+    }
+
+    /**
+     * @param $add int
+     * @param $mult int
+     */
+    public function setLineSpacing(int $add, int $mult): void {
+
+    }
+
+    /**
+     * @param $radius int
+     * @param $dx int
+     * @param $dy int
+     * @param $color int
+     */
+    public function setShadowLayer(int $radius, int $dx, int $dy, int $color): void {
+
+    }
 }
--- jphp-android-ext/src-jvm/main/java/org/venity/jphp/android/classes/widget/WrapTextView.java
+++ jphp-android-ext/src-jvm/main/java/org/venity/jphp/android/classes/widget/WrapTextView.java
@@ -1,11 +1,8 @@
 package org.venity.jphp.android.classes.widget;
 
-import android.app.Activity;
 import android.content.Context;
-import android.view.View;
 import android.widget.TextView;
 import org.venity.jphp.android.AndroidExtension;
-import org.venity.jphp.android.classes.app.WrapActivity;
 import org.venity.jphp.android.classes.view.WrapView;
 import php.runtime.annotation.Reflection;
 import php.runtime.env.Environment;
@@ -51,5 +48,36 @@ public class WrapTextView extends WrapView {
     @Reflection.Getter
     public int getInputType() {
         return getWrappedObject().getInputType();
+    }
+
+    public interface WrappedInterface {
+        @Reflection.Property int textColor();
+        @Reflection.Property int highlightColor();
+        @Reflection.Property int hintTextColor();
+        @Reflection.Property int gravity();
+        @Reflection.Property int paintFlags();
+        @Reflection.Property int minLines();
+        @Reflection.Property int minHeight();
+        @Reflection.Property int maxLines();
+        @Reflection.Property int maxHeight();
+        @Reflection.Property int minWidth();
+        @Reflection.Property int maxWidth();
+        @Reflection.Property int minEms();
+        @Reflection.Property int maxEms();
+        @Reflection.Property float textSize();
+        @Reflection.Property float textScaleX();
+        @Reflection.Property boolean freezesText();
+        @Reflection.Property boolean cursorVisible();
+        @Reflection.Property CharSequence hint();
+
+        int getShadowColor();
+        float getLineSpacingMultiplier();
+        float getLineSpacingExtra();
+        float getShadowRadius();
+        float getShadowDx();
+        float getShadowDy();
+        void append(CharSequence text, int start, int end);
+        void setLineSpacing(float add, float mult);
+        void setShadowLayer(float radius, float dx, float dy, int color);
     }
 }
